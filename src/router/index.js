2494701ffd7635ee79042f9b95425e7f3aa4263f
cjr_glb <chenjunren_glb@qq.com>
2020-02-25 11:17:23 +0800

    构建项目，配置全局样式，路由，element‘
    git commit -m构建项目，配置全局样式，路由，element‘

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Login from "../views/login/login.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    //重定向
+    redirect: "login"
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: Login
+  },
+];
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;
